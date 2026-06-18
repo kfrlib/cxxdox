3f13506ac4b1eb77e38cc44722583a74c984c474
d.levin256@gmail.com <d.levin256@gmail.com>
2026-06-18 20:22:34 +0200

    Bump version: 0.1.4 → 0.1.5

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except FileNotFoundError:
 
 setup(
     name='mkdocs-cxxdox',
-    version='0.1.4',
+    version='0.1.5',
     description='MkDocs plugin that generates C++ API documentation from source using libclang.',
     long_description=long_description,
     long_description_content_type='text/markdown',
